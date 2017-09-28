b89dfac3ec0206f6be08de3446f7f39717e3591e
yuhao <yuhaomkp@163.com>
2017-09-29 00:32:17 +0800

    csdn上看到的

--- MOOC_zju_陈越-何钦铭/01-复杂度2 Maximum Subsequence Sum.c
+++ MOOC_zju_陈越-何钦铭/01-复杂度2 Maximum Subsequence Sum.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
-int main() {
-    int isStart = 1;
-    int start1 = 0,end1 = 0;
-    int start = 0, end = 0;
-    int i = 0, j = 0, K = 0;
-    int MaxSum = 0, ThisSum = 0;
-    scanf("%d", &K);
-    if (K == 1){
-        scanf("%d",&j);
-        start = j;
-        end = j;
-        MaxSum = j;
+int a[100005];
+int main()
+{
+    int temp,k,start,end1,sum;
+    sum=-1;
+    scanf("%d",&k);
+    for(int i=0;i<k;i++){
+        scanf("%d", &a[i]);
     }
-    else {
-        for (i = 0; i < K; i++) {
-            scanf("%d", &j);
-            if (i == 0) start1 = j;
-            if (i == K-1) end1 = j;
-            ThisSum += j;
-            if (ThisSum >= MaxSum) {
-                if (ThisSum > MaxSum) end = j;
-                MaxSum = ThisSum;
-                if (isStart) {
-                    start = j;
-                    isStart = 0;
-                }
-            } else if (ThisSum < 0) {
-                ThisSum = 0;
-                isStart = 1;
+    for(int i=0;i<k;i++){
+        temp=0;
+        for(int j=i;j<k;j++){
+            temp=temp+a[j];
+            if(temp>sum){
+                sum=temp;
+                start=a[i];
+                end1=a[j];
             }
         }
     }
-    if (MaxSum > 0)
-        printf("%d %d %d",MaxSum,start,end);
+    if(sum<=-1)
+        printf("0 %d %d",a[0],a[k-1]);
     else
-        printf("0 %d %d", start1, end1);
+        printf("%d %d %d",sum, start, end1);
+    
+    
     return 0;
 }
