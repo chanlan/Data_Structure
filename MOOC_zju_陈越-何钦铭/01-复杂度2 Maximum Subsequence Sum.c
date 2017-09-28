c25b925851d9eafa19222cef2e1c255d12140820
yuhao <yuhaomkp@163.com>
2017-09-28 21:23:37 +0800

    bug在Thissum等于maxsum时start变了

--- MOOC_zju_陈越-何钦铭/01-复杂度2 Maximum Subsequence Sum.c
+++ MOOC_zju_陈越-何钦铭/01-复杂度2 Maximum Subsequence Sum.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <stdbool.h>
-
 int main() {
-    bool isStart = true;
+    int isStart = 1;
+    int start1 = 0,end1 = 0;
     int start = 0, end = 0;
     int i = 0, j = 0, K = 0;
     int MaxSum = 0, ThisSum = 0;
-
     scanf("%d", &K);
     if (K == 1){
         scanf("%d",&j);
@@ -17,23 +15,25 @@ int main() {
     else {
         for (i = 0; i < K; i++) {
             scanf("%d", &j);
+            if (i == 0) start1 = j;
+            if (i == K-1) end1 = j;
             ThisSum += j;
-            if (ThisSum > MaxSum) {
+            if (ThisSum >= MaxSum) {
+                if (ThisSum > MaxSum) end = j;
                 MaxSum = ThisSum;
-                end = i;
-                if (isStart && i > 0) {
-                    start = i;
-                    isStart = false;
+                if (isStart) {
+                    start = j;
+                    isStart = 0;
                 }
             } else if (ThisSum < 0) {
                 ThisSum = 0;
-                isStart = true;
+                isStart = 1;
             }
         }
     }
     if (MaxSum > 0)
         printf("%d %d %d",MaxSum,start,end);
     else
-        printf("0 %d %d" ,start,end);
+        printf("0 %d %d", start1, end1);
     return 0;
 }
