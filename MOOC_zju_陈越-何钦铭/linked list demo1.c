bff1c85a3455a7c6e008842944cfe000c1fbbb33
mkp666 <yuhaomkp@163.com>
2017-10-03 21:04:45 +0800

    the print function has bug

--- MOOC_zju_陈越-何钦铭/linked list demo1.c
+++ MOOC_zju_陈越-何钦铭/linked list demo1.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 typedef struct node *list;
 struct node {
-    int date;
+    int data;
     list next;
-}
+};
 
 list head;
 list creat();
 void print (list);
 int length(list);
-void search(list));
+void search(list);
 void sort(list);
 void add(list);
 void del(list);
 void reverse(list);
 
-int int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) {
     int ch;
     head = creat();
     print(head);
@@ -37,15 +37,15 @@ int int main(int argc, char const *argv[]) {
     return 0;
 }
 list creat(){
-    list new_one, head, prec tmp;
-    in i, n;
+    list new_one, head, prev;
+    int i, n;
     head = NULL;
     printf("\nHow many nodes ?:");
     scanf("%d", &n);
     for (i = 0; i < n; i++) {
         new_one = (list)malloc(sizeof(struct node));
         new_one->next = NULL;
-        printf("\nEnter data:", );
+        printf("\nEnter data:");
         scanf("%d",&new_one->data);
         if (head == NULL) {
             head = new_one;
@@ -74,12 +74,12 @@ void search(list head){
     printf("\nEnter Number:");
     scanf("%d", &key);
     while (tmp) {
-        if (tmp->date == key) {
-            printf("Found\n", );
-            printf("<-%d->", tmp->date);
+        if (tmp->data == key) {
+            printf("Found\n");
+            printf("<-%d->", tmp->data);
             is = 0;
         }
-        tmp = tmp->date;
+        tmp = tmp->next;
     }
     if (is) printf("NOT FOUNd");
 }
@@ -87,12 +87,12 @@ void sort(list head) {
     list tmp, tmp_one;
     tmp = head;
     int n=0, i, j, cur;
-    n = length(list);
+    n = length(head);
     for (i = 0; i<n-1; i++) {
         tmp = head;
         for(j = 0; j < n-1-i; j++){
             tmp_one = tmp->next;
-            if(tmp->data > tmpe_one->data){
+            if(tmp->data > tmp_one->data){
                 cur = tmp->data;
                 tmp->data = tmp_one->data;
                 tmp_one->data = cur;
@@ -101,50 +101,57 @@ void sort(list head) {
         }
     }
 }
-void del(list head) {
-    list tmp,tmp_one;
-    int del_key,k;
-    printf("\n1.Delete begin\n2.Delete at end\n3.Delete in between");
-    scanf("%d",&k);
-   switch(k)
-    {
-    case 1: temp_one=head;
-    temp=head->next;
-    head=temp;
-    temp_one->next=NULL;
-    free(temp_one);
-    break;
-    case 2:
-    temp=head;
-       while(temp->next!=NULL)
-         {
-          temp_one=temp;
-          temp=temp->next;
-         }
-          temp_one->next=NULL;
-          free(temp);
-       break;
-    case 3:
-    printf("\nEnter node which you want to delete:");
-    scanf("%d",&del_key);
-     temp=head;
-     while(temp!=NULL)
-       {
-         if(temp->data==del_key)
-     {
-      temp_one->next=temp->next;
-      temp->next=NULL;
-      free(temp);
-     }
-    temp_one=temp;
-    temp=temp->next;
-       }
-    break;
-     }
+void del(list head){
+    list tmp , tmp_one;
+    int del_key, key;
+    printf("1.Delete begin\n2.Delete end\n3.Delete k\n");
+    scanf("%d",&key);
+    switch (key) {
+        case 1:
+            tmp_one = head;
+            tmp = head->next;
+            head = tmp;
+            tmp_one->next = NULL;
+            free(tmp_one);
+            break;
+        case 2:
+            tmp = head;
+            while (tmp->next != NULL) {
+                tmp_one = tmp;
+                tmp = tmp->next;
+            }
+            tmp_one->next = NULL;
+            free(tmp_one);
+        case 3:
+            printf("Enter the key to del\n");
+            scanf("%d", &del_key);
+            tmp = head;
+            while (tmp != NULL) {
+                if (tmp->data == del_key) {
+                    tmp_one->next = tmp->next;
+                    tmp->next = NULL;
+                    free(tmp);
+                }
+                tmp_one = tmp;
+                tmp = tmp->next;
+            }
+            break;
+    }
 }
-
+void reverse(list head){
+    list tmp, tmp_one, tmp_two;
+    tmp = head;
+    tmp_one = tmp->next;
+    tmp->next = NULL;
+    while (tmp_one != NULL) {
+        tmp_two = tmp_one->next;
+        tmp_one->next = tmp;
+        tmp = tmp_one;
+        tmp_one = tmp_two;
+    }
+    head = tmp;
 }
-void length(list head){
+int length(list head){
     list tmp;
     int i = 0;
     tmp = head;
@@ -153,4 +160,49 @@ void length(list head){
         i++;
     }
     return i;
+}
+void add(list head){
+    list tmp, tmp_one, tmp_two;
+    int key, add_key;
+    tmp_two = (list)malloc(sizeof(struct node));
+    printf("Enter data for new node to be added\n");
+    scanf("%d",&tmp_two->data);
+    tmp_two->next = NULL;
+    printf("\n1.Add begin\n2.Add end\n3.Add in key\n");
+    scanf("%d", &key);
+    switch(key){
+        case 1:
+            tmp = head;
+            head = tmp_two;
+            head->next = tmp;
+            break;
+        case 2:
+            if(head == NULL){
+                tmp = head;
+                head = tmp_two;
+                tmp_two->next = NULL;
+            }
+            else {
+                tmp = head;
+                while (tmp->next != NULL){
+                    tmp = tmp->next;
+                }
+                tmp->next = tmp_two;
+                tmp = tmp_two;
+            }
+            break;
+        case 3:
+            printf("\nEnter value of node after which you want to add new node");
+            scanf("%d",&add_key);
+            tmp = head;
+            while(tmp!=NULL){
+                if(tmp->data == add_key){
+                    tmp_one = tmp->next;
+                    tmp->next = tmp_two;
+                    tmp_two->next = tmp_one;
+                }
+            tmp = tmp->next;
+            }
+        break;
+    }
 }
