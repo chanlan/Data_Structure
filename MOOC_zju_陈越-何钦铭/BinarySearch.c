f73fc82367e740b66d38349b0b02f5d5e87fcbcd
mkp666 <yuhaomkp@163.com>
2017-10-04 19:12:58 +0800

    Pseudocode BinarySearch

---
+++ MOOC_zju_陈越-何钦铭/BinarySearch.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define ElementType (int)
+
+int BinarySearch(List Tbl, ElementType K){
+    int left, right, mid, NoFound = -1;
+    left = 1;
+    right = Tbl->Length;
+    while (left < right) {
+        mid = (left + right)/2;
+        if (K < Tbl->Element[mid])
+            
+    }
+}
