fe26098c4aa0bf8d5312109f0128d9f1feebd1fe
yuhao <yuhaomkp@163.com>
2017-10-20 20:04:41 +0800

    YES!

--- MOOC_zju_陈越-何钦铭/03-树2 List Leaves.c
+++ MOOC_zju_陈越-何钦铭/03-树2 List Leaves.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-
 typedef struct TNode TNode;
 struct TNode {
     int v;
@@ -10,15 +8,15 @@ struct TNode {
 }array[10];
 
 int main(){
-    int i = 0;
+    int i = 0,k;
     int n, Root;
     int check[10];
     int num[10];
     for (int j = 0; j < 10; ++j) check[j] = 0;
     char n1,n2;
-    scanf("%d\n", &n);
+    scanf("%d", &n);
     for(i=0; i<n;i++){
-        scanf("%c %c\n", &n1, &n2);
+        scanf("\n%c %c", &n1, &n2);
         array[i].v = i;
         if(n1 != '-') {
             array[i].Left = n1 - '0';
@@ -33,9 +31,9 @@ int main(){
         else
             array[i].Right = -1;
     }
-    int k;
-    for ( k = 0; check[k]; ++k);
+    for(k =0; check[k]; k++);
     Root = k;
+
     TNode queue[10], Node;
     int front=0,rear = -1;
     queue[++rear] = array[Root];
@@ -44,10 +42,12 @@ int main(){
         Node = queue[front++];
         if(Node.Left == -1 && Node.Right == -1)
             num[i++]=Node.v;
-        else if(Node.Left != -1)
-            queue[++rear]=array[Node.Left];
-        else if(Node.Right != -1)
-            queue[++rear]=array[Node.Right];
+        else {
+            if (Node.Left != -1)
+                queue[++rear] = array[Node.Left];
+            if (Node.Right != -1)
+                queue[++rear] = array[Node.Right];
+        }
     }
     bool isFirst = 1;
     for(int j=0; j< i; j++){
