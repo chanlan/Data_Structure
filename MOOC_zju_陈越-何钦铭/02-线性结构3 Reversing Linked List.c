c037a867ecd8858c8080fbc85e5d49c53dc2127e
yuhao <yuhaomkp@163.com>
2017-10-21 17:48:36 +0800

    unfinished

--- MOOC_zju_陈越-何钦铭/02-线性结构3 Reversing Linked List.c
+++ MOOC_zju_陈越-何钦铭/02-线性结构3 Reversing Linked List.c
@@ -1,90 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 typedef struct Node* LNode;
 struct Node{
-    int Adress;
-    int value;
-    int next;
-    LNode link;
-};
-LNode revers( LNode head, int K, int N){
-    int cnt =1;
-    LNode tmp,old,new;
-    new = head->link;
-    old = new ->link;
-    while(cnt < K){
-        tmp = old->link;
-        old->link = new;
-        new = old;
-        old = tmp;
-        cnt++;
-    }
-    head->link->link = old;
-    return new;
-}
-int main() {
-    LNode head;
-    head = (LNode)malloc(sizeof(struct Node));
-    head->link = NULL;
-    int first, i, N, K;
-    scanf("%d %d %d",&first, &N, &K);
-    for (i = 0; i < N; i++) {
-        LNode tmp = head;
-        LNode node = (LNode)malloc(sizeof(struct Node));
-        scanf("\n%d %d %d", &node->Adress, &node->value, &node->next);
-        node->link = NULL;
-        while (tmp->link != NULL) {
-            tmp = tmp->link;
-        }
-        tmp->link = node;
-    }
-    //find the first
-    LNode tmp = head->link;
-    LNode tmp1 = tmp;
-    LNode tmp2;
-    i = 0;
-    while(tmp->Adress != first){
-        tmp1 = tmp;
-        tmp = tmp->link;
-        i = 1;
-    }
-    if(i == 1){
-        tmp1->link = tmp->link;
-        tmp->link = head->link;
-        head->link = tmp;
-    }
-// It is a bad idea!! SORT
-    int count = 0;
-    tmp = head->link;
-    while(count < N-1) {
-        if (tmp->next == tmp->link->Adress) {
-            tmp = tmp->link;
-            count++;
-        } else {
-            tmp1 = tmp->link->link;
-            i = 0;
-            while (tmp->next != tmp1->Adress) {
-                tmp2 = tmp1;
-                tmp1 = tmp1->link;
-                i = 1;
-            }
-            if (i == 1) {
-                tmp2->link = tmp1->link;
-                tmp1->link = tmp->link;
-                tmp->link = tmp1;
-                tmp = tmp->link;
-            } else {
-                tmp->link->link = tmp1->link;
-                tmp1->link = tmp->link;
-                tmp->link = tmp1;
-                tmp = tmp->link;
-            }
-            count++;
-        }
-    }
-    //Reversing Linked List
-    head = revers(head, K, N);
+    int address;
+    int data;
+    int nextAddress;
+    struct Node * next;
+}Node;
 
+int main() {
+    LNode List, tmpList, tmpNode, tmp, tmp1;
+    List = (LNode) malloc(sizeof(Node));
+    List->next = NULL;
+    int firstAdress;
+    int i, N, K;
+    scanf("%d %d %d", &firstAdress, &N, &K);
+    tmpList = List;
+    for (i = 0; i < N; i++) {
+        tmpNode = (LNode) malloc(sizeof(Node));
+        scanf("%d %d %d", &tmpNode->address, &tmpNode->data, &tmpNode->nextAddress);
+        tmpNode ->next = NULL;
+        tmpList->next = tmpNode;
+        tmpList = tmpNode;
+    }
+
+    tmp = (LNode)malloc(sizeof(struct Node));
+    tmp->next = NULL;
+    tmp1 = tmp;
+    int findAddress = firstAdress;
+    int count = 0;//有多余结点不在链表上
+    while(findAddress != -1){
+        tmpList = List;
+        while(tmpList->next){
+            if(tmpList->next->address == findAddress){
+                tmp1->next = tmpList->next;
+                tmpList->next = tmpList->next->next;
+                tmp1 = tmp1->next;
+                findAddress = tmp1->nextAddress;
+                count++;
+            }else{
+                tmpList = tmpList->next;
+            }
+        }
+    }
+    List = tmp;
+    //reversing
+    tmp = (LNode)malloc(sizeof(Node));
+    tmp->next = NULL;
+    LNode p3 = tmp;
+    LNode p2 = List;
+    LNode tail, q3;
+    int n = count;
+    int k = K;
+    while(n >= k){
+        n -= k;
+        for(i=0; i < k; i++){
+            p3->next = p2->next;
+            p2->next = p2->next->next;
+            if(i == 0)
+                tail = p3->next;
+            else
+                p3->next->next = q3;
+            q3 = p3->next;
+        }
+        p3 = tail;
+    }
+    List = tmp->next;
+    while(List->next){
+        printf("%05d %d %05d\n", List->address, List->data, List->nextAddress);
+        List = List->next;
+    }
+    printf("%05d %d -1",List->address, List->data);
     return 0;
 }
