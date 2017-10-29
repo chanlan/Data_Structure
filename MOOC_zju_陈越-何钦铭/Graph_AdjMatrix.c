8820b2895535a8138a94e7b1b8488fca104d89e2
yuhao <yuhaomkp@163.com>
2017-10-29 18:01:43 +0800

    Need to review

--- MOOC_zju_陈越-何钦铭/Graph_AdjMatrix.c
+++ MOOC_zju_陈越-何钦铭/Graph_AdjMatrix.c
@@ -1,12 +1,46 @@
 //图的临接矩阵表示法
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
+#define ElementType int
 #define MaxVertexNum 100    //最大顶点数
 #define INFINITY 65535      //最大值设为无符号整数的最大值65535
 typedef int Vertex;         //用顶点下标表示顶点
 typedef int WeightType;     //权值
 typedef char DataType;      //顶点储存的数据类型
+typedef struct Node *PtrToNode;
+struct Node {
+    ElementType Data;
+    PtrToNode Next;
+};
+typedef PtrToNode Position;
+
+struct QNode {
+    Position Front, Rear;
+    int MaxSize;
+};
+typedef struct QNode *Queue;
+bool IsEmpty( Queue Q ){
+    return ( Q->Front == NULL);
+}
+ElementType DeleteQ( Queue Q ){//? 和Queue一样的问题为啥print Q->Next->Data
+    Position FrontCell;
+    ElementType FrontElem;
+    if( IsEmpty(Q) ){
+        printf("Queue empty");
+        return 0;
+    }
+    else {
+        FrontCell = Q->Front;
+        if( Q->Front == Q->Rear )
+            Q->Front = Q->Rear = NULL;
+        else
+            Q->Front = Q->Front->Next;
+        FrontElem = FrontCell ->Data;
+        free(FrontCell);
+        return FrontElem;
+    }
+}
 //Edge Definnation
 typedef struct ENode *PtrToENode;
 struct ENode{
@@ -61,4 +95,28 @@ MGraph BuildGraph(){
     for(V=0; V<Graph->Nv; V++)
         scanf(" %c", &(Graph->Data[V]));
     return Graph;
+}
+void Visit(Vertex V){
+    printf("Vist the node %d", V);
+}
+bool IsEdge(MGraph Graph, Vertex V, Vertex W){
+    return Graph->G[V][W] < INFINITY ? true : false;
+}
+bool Visited[MaxVertexNum];
+void BFS(MGraph Graph, Vertex S, void(*Vist)(Vertex)){
+    Queue Q;
+    Vertex V, W;
+    Q = CreateQueue( MaxVertexNum );
+    Vist(S);
+    Visited[S] = true;
+    AddQ(Q, S);
+    while (!IsEmpty(Q)){
+        V = Delete(Q);
+        for (W = 0; W < Graph->Nv ; W++)
+            if(!Visited[W] && IsEdge(Graph, V, W)){
+                Visit(W);
+                Visited[W] = true;
+                AddQ(Q, W);
+            }
+    }
 }
