7d569bd8760af9c5179602fb9bc227a58180f02f
yh-cs <yuhaomkp@163.com>
2018-03-11 22:05:25 +0800

    not finished

---
+++ MOOC_zju_陈越-何钦铭/01-复杂度2 Maximum Subsequence Sum.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+
+int main(){
+    int k;
+    scanf("%d", &k);
+    int this_sum = 0, max_sum = -1;
+    int first = 0, end = 0, tmp_first = 0;
+    int flag = 0;
+    int neg_begin = 0, neg_end = 0;
+    for(int i = 0; i < k; i++){
+        int tmp;
+        scanf("%d", &tmp);
+        if(tmp >=0) flag = 1;
+
+        this_sum += tmp;
+        if(this_sum > max_sum){
+            max_sum = this_sum;
+            end = tmp;
+            first = tmp_first;
+        }
+        scanf("%d", &tmp);
+        if(this_sum < 0){
+            this_sum = 0;
+            tmp_first = tmp;
+        }
+        if(i == 0) neg_begin = tmp;
+        if(i == k-1) neg_end = tmp;
+    }
+    if(flag)
+        printf("%d %d %d", max_sum, first, end);
+    else
+        printf("0 %d %d", neg_begin, neg_end);
+    return 0;
+}
