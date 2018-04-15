5e0558d8b66229174f7c8dd37f3151acd97c959e
yuhao <yuhaomkp@163.com>
2018-04-15 23:35:52 +0800

    finish

---
+++ MOOC_pku_算法基础-郭炜/2.2N皇后问题.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int N;
+int queenPos[100];
+
+void NQueen(int k){
+	int i;
+	if(k == N){
+		for( i = 0; i < N; i++)
+			cout << queenPos[i] + 1 << " ";
+		cout << endl;
+		return ;
+	}
+	for(i = 0; i < N; i++){
+		int j;
+		for(j = 0; j < k; j++){
+			if(queenPos[j] == i || 
+				abs(queenPos[j] - i) == abs(k-j))
+				break;
+		}
+		if(j == k){
+			queenPos[k] = i;
+			NQueen(k+1);
+		}
+	}
+}
+
+int main(){
+	int n;
+	cin >> N;
+	NQueen(0);
+	system("pause");
+	return 0;
+}
---
+++ MOOC_pku_算法基础-郭炜/2.3逆波兰表达式.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdlib>
+using namespace std;
+
+double exp(){
+	char s[20];
+	cin >> s;
+	switch(s[0]){
+	case '+': return exp() + exp();
+	case '-': return exp() - exp();
+	case '*': return exp() * exp();
+	case '/': return exp() / exp();
+	default: return atof(s);
+	break;
+	}
+}
+
+int main(){
+	printf("%lf", exp());
+	return 0;
+}
