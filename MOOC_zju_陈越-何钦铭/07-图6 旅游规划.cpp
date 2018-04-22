bbc87148e6885c286f770b3f0153ccc88ce35cb6
yuhao <yuhaomkp@163.com>
2018-04-22 13:56:37 +0800

    fix

--- MOOC_zju_陈越-何钦铭/07-图6 旅游规划.cpp
+++ MOOC_zju_陈越-何钦铭/07-图6 旅游规划.cpp
@@ -1,67 +1,64 @@
 #include <cstdio>
 #include <cstdlib>
-const int MaxN = 505;
+const int MAXN = 505;
 const int INFINITY = 1000000;
+
 int N, M, S, D;
+int G[MAXN][MAXN];
+int Mo[MAXN][MAXN];
+int dist[MAXN];
+int cost[MAXN];
+int Visited[MAXN] = {false};
 
-int G[MaxN][MaxN];
-int Mo[MaxN][MaxN];
-bool visited[MaxN] = {false};
-int dist[MaxN];
-int cost[MaxN];
+void Dijkstra(int S){
+	int shortdist, v;
+	Visited[S] = true;
 
-void Dijkstra(int s){
-    int i, short1, v0;
-    visited[s] = true;
-    while(true){
-        short1 = INFINITY;
-        v0 = -1;
-        for(i = 0; i < N; i++){
-            if(!visited[i] && dist[i] < short1){
-                v0 = i;
-                short1 = dist[i];
-            }
-        }
-        if(v0 <0) break;
-        visited[v0] = true;
-        for(i = 0; i < N; i++){
-            if(!visited[i] && G[v0][i]+ dist[v0] < dist[i]){
-                dist[i] = G[v0][i] + dist[v0];
-                cost[i] = cost[v0] + Mo[v0][i];
-            }else if(dist[v0] + G[v0][i] == dist[i] && cost[v0] + Mo[v0][i] < cost[i]){
-                cost[i] = cost[v0] + Mo[v0][i];
-            }
-        }
-    }
-}
-
-int main(){
-	scanf("%d %d %d %d", &N, &M, &S, &D);
-
-	int i, j;
-	for(i = 0; i < N; i++){
-        for(j = 0; j < N; j++){
-            if(i == j){
-                G[i][j] = 0;
-                Mo[i][j] = 0;
-            }
-            else{
-                G[i][j] = INFINITY;
-                Mo[i][j] = INFINITY;
-            }
-        }
+	while(true){
+		shortdist = INFINITY;
+		v = -1;
+		for(int i = 0; i < N; i++)
+			if(!Visited[i] && dist[i] < shortdist){
+				v = i;
+				shortdist = dist[i];
+			}
+		if(v == -1) break;
+		Visited[v] = true;
+		for(int i = 0; i < N; i++){
+			if(!Visited[i] && dist[v] + G[v][i] < dist[i]){
+				dist[i] = G[v][i] + dist[v];
+				cost[i] = cost[v] + Mo[v][i];
+			}else if(!Visited[i] && dist[v] + G[v][i] == dist[i] && cost[i] > Mo[v][i] + cost[v]){
+				cost[i] = Mo[v][i] + cost[v];
+			}
+		}
 	}
+}
+int main(){
+    scanf("%d %d %d %d", &N, &M, &S, &D);
+    int i, j;
+    for(i = 0; i < N; i++)
+        for(j = 0; j < N; j++){
+			if(i == j){
+				G[i][j] = 0;
+				Mo[i][j] = 0;
+			}else{
+				G[i][j] = INFINITY;
+				Mo[i][j] = INFINITY;
+			}
+		}
 	int c1, c2, l, m;
 	for(i = 0; i < M; i++){
-		scanf("%d %d %d %d",&c1, &c2, &l, &m);
+		scanf("%d %d %d %d", &c1, &c2, &l, &m);
 		G[c1][c2] = G[c2][c1] = l;
 		Mo[c1][c2] = Mo[c2][c1] = m;
 	}
-    for(i = 0; i < N; i++){
-        dist[i] = G[S][i];
-        cost[i] = Mo[S][i];
-    }
-    Dijkstra(S);
-    printf("%d %d\n", dist[D], cost[D]);
+	for( i = 0; i < N; i++){
+		dist[i] = G[S][i];
+		cost[i] = Mo[S][i];
+	}
+	Dijkstra(S);
+	printf("%d %d\n", dist[D], cost[D]);
+	system("pause");
 	return 0;
 }
